1ceefdbb3946c3fb27c417721d61dd4d084af1c0
brianskiies <brian.he3@gmail.com>
2021-05-06 10:15:38 +1000

    added event listener to button

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { getArtistData, getAlbumData } from "./utils.js"
 
+const searchElement = document.getElementById("search")
+const getArtistButton = document.getElementById("get-artist-button")
+getArtistButton.addEventListener('click', () => getArtistData(searchElement))
+
 getArtistData('Cher')
 
-getAlbumData('Cher')
+getAlbumData('Cher')    
