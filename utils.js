96c1c4efbf867bc1fe02e47a3a655e509bb40220
gjoyner09 <gjoyner09@gmail.com>
2021-05-06 12:24:44 +1000

    added functionality to show name and link for similar artists

--- utils.js
+++ utils.js
@@ -11,6 +11,7 @@ export async function getArtistData() {
     let artistData = await artistResponse.json()
     console.log(artistData)
     displayArtistData(artistData)
+    artistData.artist.similar.artist.forEach((simArtist, index) => { displaySimilarData(simArtist, index) })
 }
 
 export async function getAlbumData(artist) {
@@ -21,26 +22,42 @@ export async function getAlbumData(artist) {
 }
 
 function displayArtistData(artistData) {
-    const artistDiv = document.getElementById("artist-info")
-    if (artistDiv.children.length === 0) {
-        artistHeader = document.createElement("p")
-        artistDiv.appendChild(artistHeader)
-        artistPic = document.createElement("img")
-        artistDiv.appendChild(artistPic)
-        artistName = document.createElement("p")
-        artistDiv.appendChild(artistName)
-        artistBio = document.createElement("p")
-        artistDiv.appendChild(artistBio)
-        artistLink = document.createElement("a")
-        artistDiv.appendChild(artistLink)
+    try {
+        const artistDiv = document.getElementById("artist-info")
+        if (artistDiv.children.length === 0) {
+            artistHeader = document.createElement("p")
+            artistDiv.appendChild(artistHeader)
+            artistPic = document.createElement("img")
+            artistDiv.appendChild(artistPic)
+            artistName = document.createElement("p")
+            artistDiv.appendChild(artistName)
+            artistBio = document.createElement("p")
+            artistDiv.appendChild(artistBio)
+            artistLink = document.createElement("a")
+            artistDiv.appendChild(artistLink)
+        }
+        artistHeader.textContent = `If you like ${artistData.artist.name}...`
+        let albumData = getAlbumData(artistData.artist.name)
+        albumData.then(result => {
+            artistPic.src = result.topalbums.album[0].image[2]["#text"]
+        })
+        artistName.textContent = artistData.artist.name
+        artistBio.textContent = artistData.artist.bio.content.slice(0, 500)
+        artistLink.href = artistData.artist.url
+        artistLink.textContent = "Link to artist on last.fm"
+    } catch (iFuckedUp) {
+        alert("Your artist was not found. Please try again.")
     }
-    artistHeader.textContent = `If you like ${artistData.artist.name}...`
-    let albumData = getAlbumData(artistData.artist.name)
-    albumData.then(result => {
-        artistPic.src = result.topalbums.album[0].image[2]["#text"]
-    })
-    artistName.textContent = artistData.artist.name
-    artistBio.textContent = artistData.artist.bio.content.slice(0, 500)
-    artistLink.href = artistData.artist.url
-    artistLink.textContent = "Link to artist on last.fm"
+}
+
+function displaySimilarData(simArtist, index) {
+    const simDiv = document.getElementById("search-results")
+    const simHeader = document.getElementById("sim-artist-header")
+    simHeader.textContent = "You may also like..."
+        // let simArtistDiv = document.getElementById(`artist${index}`)
+    let simNameP = document.getElementById(`name${index}`)
+    simNameP.textContent = simArtist.name
+    let simLinkA = document.getElementById(`a${index}`)
+    simLinkA.href = simArtist.url
+    simLinkA.textContent = "Link to artist on last.fm"
 }
